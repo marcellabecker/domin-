7252374997163bda96d8711d2654961bae65a15e
marcellabecker <54988365+marcellabecker@users.noreply.github.com>
2019-10-17 20:33:35 -0300

    Create GameStructures.h

---
+++ GameStructures.h
@@ -0,0 +1,17 @@
+# ifndef _structs
+# define  _structs
+
+# include  " Lista.h "
+# include  " Pilha.h "
+
+typedef  struct {
+	tp_list_se * mao;
+	char * nome;
+jogador;
+
+typedef  struct {
+	tp_list_se * peecas;
+	pilha monte;
+mesa;
+
+# endif
